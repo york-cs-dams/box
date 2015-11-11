045b3a6633370904b3c9c29149cdc29a2e61c1e3
Louis Rose <louismrose@gmail.com>
2015-11-11 11:18:41 +0000

    Add MySQL DB.

--- scripts/custom/db.sh
+++ scripts/custom/db.sh
@@ -3,15 +3,15 @@
 PASSWORD=root
 
 echo "Installing MySQL"
-export DEBIAN_FRONTEND=noninteractive
-sudo apt-get install --yes mysql-server
+sudo DEBIAN_FRONTEND=noninteractive apt-get --yes install mysql-server
 sudo mysqladmin -u root password $PASSWORD
 
 echo "Configuring MySQL"
-mysql -u root --password=$PASSWORD --execute="DROP USER ''@'localhost'"
-mysql -u root --password=$PASSWORD --execute="DROP USER ''@'$(hostname)'"
-mysql -u root --password=$PASSWORD --execute="DROP DATABASE test"
+mysql -u root --password=$PASSWORD --execute="SET PASSWORD FOR 'root'@'$(hostname)' = password('$PASSWORD');"
+mysql -u root --password=$PASSWORD --execute="SET PASSWORD FOR 'root'@'127.0.0.1' = password('$PASSWORD');"
+mysql -u root --password=$PASSWORD --execute="SET PASSWORD FOR 'root'@'::1' = password('$PASSWORD');"
 mysql -u root --password=$PASSWORD --execute="FLUSH PRIVILEGES"
 
 echo "Creating databases"
 mysql -u root --password=$PASSWORD --execute="CREATE DATABASE flippd"
+mysql -u root --password=$PASSWORD --execute="CREATE DATABASE flippd_test"
